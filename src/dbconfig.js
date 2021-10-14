4485399a657b3597faef1ee9c9b8e8fb6fcd1848
Vittoria Cassoni <vittoriacassoni@outlook.com>
2021-10-14 18:05:02 -0300

    Add .env to gitignore

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -1,10 +1,9 @@
 const config = {
-  server: 'ftt-violence-mapping.database.windows.net', // Use your SQL server name
-  database: 'ftt_violence_mapping', // Database to connect to
-  user: 'azureuser', // Use your username
-  password: '&@9!Y!Rq6u*L', // Use your password
+  server: 'ftt-violence-mapping.database.windows.net',
+  database: 'ftt_violence_mapping',
+  user: 'azureuser',
+  password: '&@9!Y!Rq6u*L',
   port: 1433,
-  // Since we're on Windows Azure, we need to set the following options
   options: {
     encrypt: true,
   },
