db69e94dbeb57cbdf8f824104136f8ce6665f012
Vittoria Cassoni <vittoriacassoni@outlook.com>
2021-10-14 18:39:42 -0300

    Adding JWT secret in token generation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
 const { Router } = require('express');
 const routes = Router();
 const jwt = require('jsonwebtoken');
-const secretKey = 'testando Key';
+
+require('dotenv').config();
 
 const UserRouter = require('./UserRouter');
 
 routes.use('/user', UserRouter);
 
-routes.use('/login',(req, res) => {
-    var id = 1;
-    const token = jwt.sign({id},secretKey,{expiresIn: 300});
-    return res.json({token:token});
+routes.use('/login', (req, res) => {
+  var id = 1;
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: 300 });
+  return res.json({ token: token });
 });
 
 module.exports = routes;
